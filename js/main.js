1ab376a8ce449b53eaabc4ff4f8387c35458ab9f
Alanna Celentano <alannacelentano@Alannas-MacBook-Pro.local>
2020-02-02 10:35:01 -0600

    add renderMessage function

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@
 /*------Constants------*/
 
 const WIN_CONDITIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6], [0, 3, 6], [1, 4, 7], [2, 5, 8]];
+const colors = {
+    "1": "red",
+    "-1": "blue",
+    "null": "white",
+}
 
 /*------Variables (state)------*/
 
@@ -9,12 +14,7 @@ const WIN_CONDITIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6], [
 
 let board;
 let turn;
-let playerOne;
-let playerTwo;
-let playerOneMoves;
-let PlayerTwoMoves;
 let winner;
-let availableSpaces;
 
 
 
@@ -22,12 +22,22 @@ let availableSpaces;
 
 // You might choose to put your game status here
 const messageEl = document.getElementById("message");
-const boardEl = document.querySelectorAll(".board");
+let squares = {
+    square0: document.getElementById("sq0"),
+    square1: document.getElementById("sq1"),
+    square2: document.getElementById("sq2"),
+    square3: document.getElementById("sq3"),
+    square4: document.getElementById("sq4"),
+    square5: document.getElementById("sq5"),
+    square6: document.getElementById("sq6"),
+    square7: document.getElementById("sq7"),
+    square8: document.getElementById("sq8"),
+}
 
 
 /*------Event Listeners------*/
-
-boardEl.addEventListener("click", whenClicked);
+// document.querySelectorAll(".board").addEventListener("click", whenClicked);
+// squareEl.addEventListener("click", squareClick);
 // This is where you should put the event listener
 // for a mouse-click
 
@@ -36,48 +46,34 @@ boardEl.addEventListener("click", whenClicked);
 init(); 
 
 // Some functions you might choose to use:
-
-function init() {
-
-
 // Initialization function:
 // Where you set your initial state, setting up 
 // what the board will look like upon loading
-    messageEl.textContent = "Let's play Tic-Tac-Toe!";
-    playerOneMoves = [];
-    playerTwoMoves = [];
-    turn = 0;
-    clearBoard();
-    nextTurn();
-    render();
 
-
-}
-
-function nextTurn() {
-    turn = 0;
-
-
+function init() {
+    board = [null, null, null, null, null, null, null, null, null];
     turn = 1;
-
-}
-
-function whenClicked(event) {
-    if (event.target.tagName !== "div" || checkWinner())
-    return;
-    let selection = event.target.tagName;
-    //how to make selection = to id??
-    selection.textContent = "X";
-
-
+    winner = null;
     render();
 }
+
+
+
 // On-Click function:
 // Set up what happens when one of the elements
 // is clicked
 
-function checkWinner() {
-
+function squareClick(event) {
+   
+    let selection = this;
+    if (selection.tagName !== "div" || checkWinner() || turn === -1)
+    return;
+    selection.textContent = "X";
+    console.log(selection);
+    playerOneMoves.push(selection);
+    //how to make selection = to id??
+    turn = -1;
+    render();
 }
 
 // Check winner function:
@@ -85,33 +81,51 @@ function checkWinner() {
 // a winner and changes the state of the winner
 // variable if so
 
+function checkWinner() {
+        
+    
+}
+    
+    // Render function:
+    // Displays the current state of the board
+    // on the page, updating the elements to reflect
+    // either X or O depending on whose turn it is
+    
 function render() {
+//iterate through square obj to make each square = specific board index
+        board.forEach(function(square) {
+            
+        });
+    
+    console.log(board);
 
     renderMessage();
 }
-// Render function:
-// Displays the current state of the board
-// on the page, updating the elements to reflect
-// either X or O depending on whose turn it is
-
+    
 function renderMessage() {
-    if (playerOne === winner) {
-        messageEl.textContent = "You Win!";
-    }
-    else if (playerTwo === winner) {
-        messageEl.textContent = "You Lose :(";
-    }
-
-    else if (turn = 1) {
-        messageEl.textContent = "Player One's Turn";
+    if (winner === null) {
+        if (turn === 1) {
+            messageEl.textContent = "Your turn, Player One";
+        } //switch this to a ternary statement!
+        if (turn === -1) {
+            messageEl.textContent = "Your turn, Player Two";
+        }
     }
 
-    else (turn = - 1) {
-        messageEl.textContent = "Player Two's Turn";
+    else if (winner === 1) {
+        messageEl.textContent = "Congratulations, Player 1! You Win!";
+    }
+    else if (winner === -1) {
+        messageEl.textContent = "Congratulations, Player 2! You Win!";
+    }
+
+    else if (winner === "T") {
+        messageEl.textContent = "Draw!";
+    }
+    //include conditionals to change message based on turn
+    //possibly use a switch statement? Do some research first
+    else {
+        messageEl.textContent = "Let's play Tic Tac Toe!";
     }
     
-}
-
-function clearBoard() {
-    availableSpaces = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 }
